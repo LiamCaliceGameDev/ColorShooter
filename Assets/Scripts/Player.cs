cbeff739d1d5a8168068535e5f46a601cfc72acd
Liam Calice <liamcalicegamedev@gmail.com>
2016-11-07 19:07:47 +0100

    Added Player Idle Animation & Turning

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+	void Update () {
+
+		Turn ();
+
+	}
+
+	void Turn () {
+		if (Input.GetKeyDown (KeyCode.UpArrow)) {
+			transform.rotation = Quaternion.Euler (Vector3.zero);
+		} else if (Input.GetKeyDown (KeyCode.DownArrow)) {
+			transform.rotation = Quaternion.Euler (Vector3.down * 180);
+		} else if (Input.GetKeyDown (KeyCode.RightArrow)) {
+			transform.rotation = Quaternion.Euler (Vector3.down * -90);
+		} else if (Input.GetKeyDown (KeyCode.LeftArrow)) {
+			transform.rotation = Quaternion.Euler (Vector3.down * 90);
+		}
+	}
+
+}
